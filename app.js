f766388e8be6e5691a076d19adb689d541047bff
laith <mr.layth_2011@hotmail.com>
2020-07-02 00:16:41 +0300

    finish

--- app.js
+++ app.js
@@ -2,15 +2,21 @@
 $(document).ready(function() {
 
     $(window).resize(function() {
-        if ($(window).width() >= 768 && $(window).width() <= 960) {
+        if ($(window).width() >= 1281) {
             console.log('desktop');
             $("div").css("background-color", "green");
             $("aside").css("background-color", "green");
         }
-        if ($(window).width() >= 480 && $(window).width() <= 767) {
+        if ($(window).width() >= 320 && $(window).width() <= 480) {
             $("div").css("background-color", "red");
             $("aside").css("background-color", "red");
             console.log('mobile');
         }
+        if ($(window).width() >= 768 && $(window).width() <= 1024) {
+            $("div").css("background-color", "yellow");
+            $("aside").css("background-color", "yellow");
+
+        }
+
     });
 });
